425ce9f73b0c20648f0139ef5a44c4798156468d
Gourd <Gourd112@proton.me>
2025-01-27 12:25:19 -0500

    StudentInfo Class SetUp

---
+++ RecordExample/src/main/java/org/example/StudentInfo.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class StudentInfo {
+    private String firstName;
+    private String lastName;
+    private int birthMonth;
+    private int birthDay;
+    private int birthYear;
+    private int StudentID;
+    private String major;
+    private String address;
+    private String city;
+    private String state;
+    private String zipCode;
+    private String country;
+}
